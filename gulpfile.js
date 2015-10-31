b8ca2ee3204b39cf11076e3e0f278b321d3f7480
Joe Hildebrand <joe-github@cursive.net>
2015-10-31 13:08:16 +0900

    Watch working

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,19 @@
 'use strict';
 
 var gulp = require('gulp'),
+    gutil = require('gulp-util'),
     mocha = require('gulp-mocha'),
     istanbul = require('gulp-istanbul');
 
-var TESTS = ['./test/**/*.js'];
+var TESTS = './test/**/*.js';
+var SRC = './lib/**/*.js';
 
 gulp.task('lint', function() {
   var eslint = require('gulp-eslint');
 
   return gulp.src([
-    'lib/**/*.js',
-    'test/**/*.js',
+    SRC,
+    TESTS,
     'gulpfile.js'
   ])
   .pipe(eslint())
@@ -20,26 +22,31 @@ gulp.task('lint', function() {
 
 gulp.task('test', function() {
   var mocha = require('gulp-mocha');
-  return gulp.src(TESTS)
+  return gulp.src([TESTS])
     .pipe(mocha());
 });
 
 gulp.task('pre-coverage', function() {
-  return gulp.src([ 'lib/*.js' ])
+  return gulp.src([SRC])
     .pipe(istanbul())
     .pipe(istanbul.hookRequire());
-})
+});
 
 gulp.task('coverage', [ 'pre-coverage' ], function() {
-  return gulp.src(TESTS)
+  var t = gulp.src([TESTS])
   .pipe(mocha().on('error', function(er) {
-    gutil.log(er.stack);
+    gutil.log(er);
     t.end();
   }))
   .pipe(istanbul.writeReports({
     dir: './coverage/nodejs',
     reporters: ['html', 'text-summary']
   }));
-})
+  return t;
+});
+
+gulp.task('watch', ['coverage'], function() {
+  return gulp.watch([SRC, TESTS], ['coverage']);
+});
 
 gulp.task('default', ['coverage']);
--- test/index-test.js
+++ test/index-test.js
@@ -6,5 +6,6 @@ var icebox = require('../');
 describe('Public API', function() {
   it('loads', function() {
     assert.ok(icebox);
+    assert.equal(icebox.foo(), 'foot');
   });
 });
