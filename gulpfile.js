c39a31b4765b937a266dec9865352122fbcd9650
Joe Hildebrand <joe-github@cursive.net>
2015-10-31 12:48:36 +0900

    Keep roughing out

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+"use strict";
+
+var gulp = require('gulp'),
+    mocha = require("gulp-mocha"),
+    istanbul = require("gulp-istanbul");
+
+var TESTS = ['./test/**/*.js'];
+
+gulp.task("lint", function() {
+  var eslint = require("gulp-eslint");
+
+  return gulp.src([
+    "lib/**/*.js",
+    "test/**/*.js",
+    "gulpfile.js"
+  ])
+  .pipe(eslint())
+  .pipe(eslint.format());
+});
+
+gulp.task('test', function() {
+  var mocha = require("gulp-mocha");
+  return gulp.src(TESTS)
+    .pipe(mocha());
+});
+
+gulp.task('pre-coverage', function() {
+  return gulp.src([ 'lib/*.js' ])
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire());
+})
+
+gulp.task('coverage', [ 'pre-coverage' ], function() {
+  return gulp.src(TESTS)
+  .pipe(mocha().on('error', function(er) {
+    gutil.log(er.stack);
+    t.end();
+  }))
+  .pipe(istanbul.writeReports({
+    dir: "./coverage/nodejs",
+    reporters: ["html", "text-summary"]
+  }));
+})
+
+gulp.task('coverage', function() {
+
+});
---
+++ test/index-test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var assert = require('chai').assert;
+var icebox = require('../');
+
+describe('Public API', function() {
+  it('loads', function() {
+    assert.ok(icebox);
+  });
+});
