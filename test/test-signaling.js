8fb89b680839aa24631e1ad2209f89fd7cb04c8c
Matthew A. Miller <linuxwolf@outer-planes.net>
2015-10-31 14:42:28 +0900

    add a negative signal register test

--- test/test-signaling.js
+++ test/test-signaling.js
@@ -1,7 +1,10 @@
 'use strict';
 
 var net = require('net'),
-    through = require('through2');
+    through = require('through2'),
+    chai = require('chai');
+
+var assert = chai.assert;
 
 var signaling = require('../lib/signaling');
 
@@ -18,12 +21,26 @@ function setupMock(messages) {
 
 describe('signaling', function() {
   it('registers a user', function() {
-    var conn = setupMock("200 OK");
+    var conn = setupMock('200 OK');
     var signaler = signaling.createSignaling(conn);
     var p = signaler.register('bilbo.baggins@hobbiton.example');
     p = p.then(function() {
+      assert.ok(true);
       console.log('user registered!');
     });
     return p;
   });
+  it('fails to register', function() {
+    var conn = setupMock('403 forbidden');
+    var signaler = signaling.createSignaling(conn);
+    var p = signaler.register('bilbo.baggins@hobbiton.example');
+    p = p.then(function() {
+      assert.ok(false, 'unexpected success');
+    });
+    p = p.catch(function(err) {
+      assert.strictEqual(err.code, 403);
+      assert.strictEqual(err.message, 'forbidden');
+    });
+    return p;
+  })
 });
